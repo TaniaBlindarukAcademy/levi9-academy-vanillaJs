f0230a63dbf00c2d26c5241a0fcc67b2e4f1be00
t.blindaruk <t.blindaruk@gmail.com>
2016-11-11 15:54:27 +0200

    Show User list. Refactoring.

---
+++ js/userViewModel.js
@@ -0,0 +1,55 @@
+/**
+ * Created by t.blindaruk on 11.11.16.
+ */
+
+var UserViewModel = (function (viewModel) {
+
+    function showUser(userPartialView, user) {
+        userPartialView.getElementsByClassName('title')[0].innerHTML = user['login'];
+        userPartialView.getElementsByClassName('card-img-top')[0].src = user['avatar_url'];
+        userPartialView.getElementsByClassName('panel')[0].setAttribute('data-id', user['id']);
+        user['site_admin'] ? userPartialView.getElementsByClassName('admin')[0].innerHTML = "Admin" : null;
+        return userPartialView.innerHTML;
+    }
+
+    function toggle(element) {
+        var className = 'hidden';
+        var hasClass = element.classList.contains(className);
+        if (hasClass === true) {
+            element.classList.remove(className);
+        } else {
+            element.classList.add(className);
+        }
+    }
+
+    var PublicApi = Object.create(viewModel);
+
+    PublicApi.init = function (user) {
+        var self = this;
+        self.user = user;
+        self.htmlText = '';
+        return new Promise(function (resolve, reject) {
+            self.readPartial('partial/user.html')
+                .then(function (htmlText) {
+                    var html = document.createElement('BODY');
+                    html.innerHTML = htmlText;
+                    self.htmlDocument = html;
+                    self.htmlText = showUser(html, self.user);
+                    resolve(true);
+                }, function (error) {
+                    reject(error);
+                    alert(error);
+                });
+        })
+    };
+
+    PublicApi.getHtml = function () {
+        return this.htmlText;
+    };
+
+    PublicApi.toggleBody = function (element) {
+        toggle(element.nextElementSibling);
+    };
+
+    return PublicApi;
+})(window.app.viewModel);
--- js/viewModel.js
+++ js/viewModel.js
@@ -2,7 +2,7 @@
  * Created by tania on 10.11.16.
  */
 
-(function (api) {
+window.app.viewModel = (function () {
     var ViewModel = {
         readPartial: function (file) {
             return new Promise(
@@ -24,32 +24,5 @@
         }
     };
 
-    var PublicApi = Object.create(ViewModel);
-
-    PublicApi.userList = function () {
-        var self = this;
-        api.getUsers()
-            .then(function (users) {
-                self.readPartial('partial/userIntro.html')
-                    .then(function (partialHtml) {
-                        var userList = document.getElementById('userList');
-                        for(let i = 0;i<users.length; ++i) {
-                            var html1 = document.createElement('BODY');
-                            html1.innerHTML = partialHtml;
-                            html1.getElementsByClassName('title')[0].innerHTML = users[i]['login'];
-                            html1.getElementsByClassName('card-img-top')[0].src = users[i]['avatar_url'];
-                            users[i]['site_admin'] ? html1.getElementsByClassName('admin')[0].innerHTML = "Admin" : null;
-                            debugger;
-                            userList.innerHTML += html1.innerHTML;
-                        }
-                    });
-            });
-    };
-
-    var publicApi = Object.create(PublicApi);
-
-    publicApi.userList();
-
-    return publicApi;
-
-})(window.app.api);
+    return ViewModel;
+})();
